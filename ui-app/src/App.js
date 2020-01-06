8762ee86b2002cca1d55f467a4636dacceaff08d
diogeneskelsen <diogeneskelsen@gmail.com>
2020-01-06 16:07:49 +0000

    Update App.js
    Radar graphics

--- ui-app/src/App.js
+++ ui-app/src/App.js
@@ -3,37 +3,56 @@ import './App.css';
 import * as THREE from "three";
 
 class App extends Component {
+  
   componentDidMount() {
+    
     var scene = new THREE.Scene();
+
     var camera = new THREE.PerspectiveCamera(
-      75,
+      50,
       window.innerWidth / window.innerHeight,
       0.1,
       1000
     );
 
     var renderer = new THREE.WebGLRenderer();
-    renderer.setSize(window.innerWidth, window.innerHeight);
+        renderer.setSize(window.innerWidth, window.innerHeight);
+    
     this.mount.appendChild(renderer.domElement);
 
-    var geometry = new THREE.BoxGeometry(1, 1, 1);
-    var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-    var cube = new THREE.Mesh(geometry, material);
-    scene.add(cube);
+    camera.position.z = 50;
 
-    camera.position.z = 5;
+    //var innerRadius = 21.9;
+    //var outerRadius = 22;
+    //var segments = 100;
+    var geometry3, material3, circle;
+
+
+for (var i = 2; i < 22; i++) {
+      geometry3 = new THREE.RingBufferGeometry((i-0.1), i, 100);
+      material3 = new THREE.MeshBasicMaterial( { color: 0x42ff00 } );
+      circle = new THREE.Mesh( geometry3, material3 );
+      scene.add(circle);
+      i = i + 2;
+}
+
+    //for (var i = 10; i <= 22; (i+2)) {
+      //console.log(i);
+      /**/
+    //}
 
     var animate = function() {
       requestAnimationFrame(animate);
 
-      cube.rotation.x += 0.01;
-      cube.rotation.y += 0.01;
+      //cube.rotation.x += 0.01;
+      //cube.rotation.y += 0.01;
 
       renderer.render(scene, camera);
     };
 
     animate();
   }
+
   render() {
     return <div ref={ref => (this.mount = ref)} />;
   }
