908cc2c6a0328ce7879bb9b005c7f4556e03c832
diogeneskelsen <diogeneskelsen@gmail.com>
2020-01-07 17:41:38 +0000

    Update App.js
    + Controls
    
    Needs to smoth the acceleration

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import './App.css';
 
 import * as THREE from "three";
 import GLTFLoader from 'three-gltf-loader';
-//import {MTLLoader, OBJLoader} from 'three-obj-mtl-loader'
 
 class App extends Component {
   
@@ -12,7 +11,7 @@ class App extends Component {
     var scene = new THREE.Scene();
 
     var camera = new THREE.PerspectiveCamera(
-      75,
+      50,
       window.innerWidth / window.innerHeight,
       0.1,
       1000
@@ -23,65 +22,39 @@ class App extends Component {
     
     this.mount.appendChild(renderer.domElement);
 
-    camera.position.z = 50;
-
-    /*var geometry3, material3, circle;
-
-    for (var i = 2; i < 50; i++) {
-          geometry3 = new THREE.RingBufferGeometry((i-0.1), i, 100);
-          material3 = new THREE.MeshBasicMaterial( { color: 0x42ff00 } );
-          circle = new THREE.Mesh( geometry3, material3 );
-          //scene.add(circle);
-          i = i + 2;
-    }*/
+    //camera.position.z = 50;
 
     var geometry = new THREE.BoxGeometry( 1, 1, 1 );
     var material = new THREE.MeshBasicMaterial( { color: 0x84ff, visible: false } );
     var cube = new THREE.Mesh( geometry, material );
     scene.add( cube );
 
-var directionalLight = new THREE.DirectionalLight(0xffffff,1);
-directionalLight.position.set(7,5,-5);
-scene.add(directionalLight);
-camera.position.z = 20;
-camera.position.x = 0;
-camera.position.y = 0;
+    var directionalLight = new THREE.DirectionalLight(0xffffff,3);
+    directionalLight.position.set(7,5,5);
+    scene.add(directionalLight);
 
-var stars = [];
-var geometry5 = new THREE.BoxGeometry(1,1,1);
-var material5 = new THREE.MeshLambertMaterial({color: 0xffffff});
+    camera.position.z = 20;
+    camera.position.x = 0;
+    camera.position.y = 0;
 
-for (var i = 0; i < 10000 ; i++) {
- var rx = Math.random()*2000-1000;
- var ry = Math.random()*1000-500;
- var rz = Math.random()*10000-5000;
- stars[i] = new THREE.Mesh(geometry5,material5);
+    var stars = [];
+    var geometry5 = new THREE.BoxGeometry(1,1,1);
+    var material5 = new THREE.MeshLambertMaterial({color: 0xffffff});
 
- stars[i].position.x = rx;
- stars[i].position.y = ry;
- stars[i].position.z = rz;
- scene.add(stars[i]);
-};
+    for (var i = 0; i < 10000 ; i++) {
+     var rx = Math.random()*2000-1000;
+     var ry = Math.random()*1000-500;
+     var rz = Math.random()*10000-5000;
+     stars[i] = new THREE.Mesh(geometry5,material5);
+
+     stars[i].position.x = rx;
+     stars[i].position.y = ry;
+     stars[i].position.z = rz;
+     scene.add(stars[i]);
+    };
 
     var animate = function() {
       requestAnimationFrame(animate);
-
-      //cube.rotation.x += 0.01;
-      //cube.rotation.y += 0.01;
-      
-      /*function timeout() {
-          setTimeout(function () {
-              // Do Something Here
-              // Then recall the parent function to
-              // create a recursive loop.
-
-              //cube.position.set((cube.position.x+0.001), (cube.position.y+0.001), (cube.position.z+0.001));
-              
-              timeout();
-          }, 10);
-      }*/
-
-      //timeout();
 
       camera.position.z -= 1;
       cube.position.z -= 1; // same speed
@@ -98,29 +71,6 @@ for (var i = 0; i < 10000 ; i++) {
       renderer.render(scene, camera);
     };
 
-//let mtlLoader = new MTLLoader();
- 
-//let objLoader = new OBJLoader();
-
-
- 
-//mtlLoader.load('space-shuttle/source/ShuttleSceneH.mtl', (materials) => {
-  //materials.preload()
-  //objLoader.setMaterials(materials)
-  /*objLoader.load('ss/source/ShuttleSceneH.obj', (object) => {
-    var nave = object;
-    scene.add(nave);
-    nave.position.z -= 1;
-    var animate3 = function() {
-      requestAnimationFrame(animate3);
-      nave.position.z -= 1; // same speed
-      renderer.render(scene, camera);
-    };
-
-    animate3();
-  })*/
-//})
-
     const loader = new GLTFLoader();
 
     loader.load(
@@ -128,8 +78,6 @@ for (var i = 0; i < 10000 ; i++) {
         ( gltf ) => {
             // called when the resource is loaded
             scene.add( gltf.scene );
-
-            //console.log(gltf);
 
             gltf.scene.position.z = 0; // remove from the screen
 
@@ -143,13 +91,13 @@ for (var i = 0; i < 10000 ; i++) {
 
             var animate2 = function() {
               requestAnimationFrame(animate2);
-              if(gltf.scene.position.z > -120) {
+              if(gltf.scene.position.z > -300) { // distance from the camera
                 gltf.scene.position.z -= 1.5;
               } else {
                 gltf.scene.position.z -= 1;
               }
 
-              if(stabilization == false) {
+              if(stabilization === false) {
                 gltf.scene.position.y += 0.01;
               } 
 
@@ -157,7 +105,7 @@ for (var i = 0; i < 10000 ; i++) {
                 stabilization = true;
               }
 
-              if(stabilization == true) {
+              if(stabilization === true) {
                 gltf.scene.position.y -= 0.01;
               }
 
@@ -169,6 +117,68 @@ for (var i = 0; i < 10000 ; i++) {
             };
 
             console.log("loading done!");
+
+            var zSpeed = 0.9;
+            var xSpeed = 0.9;
+            var ySpeed = 0.9;
+
+            document.addEventListener('keydown', event => {
+                if(event.key === 'ArrowUp'){
+                  gltf.scene.position.y += ySpeed;
+                  ySpeed += 0.055;
+                }
+                if(event.key === 'ArrowLeft'){
+                  if(gltf.scene.rotation.y < 1.78) {
+                    gltf.scene.rotation.y += 0.005;
+                  }
+
+                  gltf.scene.position.x -= xSpeed;
+                  
+                  xSpeed += 0.055;
+                }
+                if(event.key === 'ArrowRight'){
+                  if(gltf.scene.rotation.y > 1.38) {
+                    gltf.scene.rotation.y -= 0.005;
+                  }
+
+                  gltf.scene.position.x += xSpeed;
+
+                  xSpeed += 0.055;
+                }
+                if(event.key === 'ArrowDown'){
+                  gltf.scene.position.y -= ySpeed;
+                  ySpeed += 0.055;
+                }
+                if(event.key === 'w'){
+                  gltf.scene.position.z -= zSpeed;
+                  zSpeed += 0.055;
+                }
+                if(event.key === 's'){
+                  gltf.scene.position.z += zSpeed;
+                  zSpeed += 0.055;
+                }
+            })
+
+            document.addEventListener('keyup', event => {
+                if(event.key === 'ArrowUp'){
+                  ySpeed = 0.9;
+                }
+                if(event.key === 'ArrowLeft'){
+                  gltf.scene.rotation.y = 1.58;
+                }
+                if(event.key === 'ArrowRight'){
+                  gltf.scene.rotation.y = 1.58;
+                }
+                if(event.key === 'ArrowDown'){
+                  ySpeed = 0.9;
+                }
+                if(event.key === 'w'){
+                  zSpeed = 0.9;
+                }
+                if(event.key === 's'){
+                  zSpeed = 0.9;
+                }
+            })
 
             animate2();
         },
