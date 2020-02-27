9f77c7df7813ac657222d34139f884f1ffcbaf4b
Diogenes Arruda <diogeneskelsen@gmail.com>
2020-02-27 14:43:00 +0000

    Update Asteroids.ts
    Refactoring  using TS

--- src/component/Asteroids.ts
+++ src/component/Asteroids.ts
@@ -1,36 +1,46 @@
-function Asteroids(scene, loader, TWEEN, _collision)
+class Asteroids
 {
-	this.asteroids = [];
-	this.tweenIds = [];
-	this.timer = 100; // first asteroid wait time to be added
+	asteroids: any = [];
+	tweenIds: any = [];
+	timer: number = 100; // first asteroid wait time to be added
+	collision: any;
+	scene: any;
+	loader: any;
+	TWEEN: any;
 
-	this.create = function(camera)
-	{
-        loader.load('/asteroids/3/scene.gltf', 
-            ( asteroid ) => {
+	constructor(scene: any, loader: any, TWEEN: any, collision: any) {
+		this.scene = scene;
+		this.loader = loader;
+		this.TWEEN = TWEEN;
+		this.collision = collision;
+	}
+
+	create(camera: any): void {
+        this.loader.load('/asteroids/3/scene.gltf', 
+            ( asteroid: any ) => {
 
 	            // Initial positions
 	            asteroid.scene.position.z = (camera.position.z + -150); // 0 to remove from the screen
 	            asteroid.scene.position.x = Math.floor(Math.random() * (50 - -50 + 1)) + -50;
 	            asteroid.scene.position.y = Math.floor(Math.random() * (50 - -50 + 1)) + -50;
 
-	            let yx = Math.random() * (1 - -1 + 0.1) + -1;
+	            let yx: any = Math.random() * (1 - -1 + 0.1) + -1;
 	            	yx = (yx < 0) ? yx.toString() : "+" + yx.toString();
 	            
-                let _asteroid_rotation_animation = new TWEEN.Tween(asteroid.scene.rotation);
+                let _asteroid_rotation_animation = new this.TWEEN.Tween(asteroid.scene.rotation);
                     _asteroid_rotation_animation.to({ y: yx, x: yx }, 1000);
                     _asteroid_rotation_animation.repeat(Infinity);
                     _asteroid_rotation_animation.start();
-                    _asteroid_rotation_animation.onRepeat(function(position) {
+                    _asteroid_rotation_animation.onRepeat(function(position: any) {
                     	position.z = asteroid.scene.position.z;
-                        _collision.setAsteroidPosition(_asteroid_rotation_animation._id, position);
+                        //this.collision.setAsteroidPosition(_asteroid_rotation_animation._id, position);
                     });
 
                 this.tweenIds[asteroid.scene.uuid] = _asteroid_rotation_animation;
 				this.asteroids[asteroid.scene.uuid] = asteroid.scene;
 
 	            // called when the resource is loaded
-	            scene.add(this.asteroids[asteroid.scene.uuid]);
+	            this.scene.add(this.asteroids[asteroid.scene.uuid]);
 
 		        // Unload asteroids outside of the screen
 		        for (let key in this.asteroids)
@@ -40,51 +50,46 @@ function Asteroids(scene, loader, TWEEN, _collision)
 			        	if(camera.position.z < this.asteroids[key].position.z) 
 			        	{
 			        		// Remove from scene
-							scene.remove(this.asteroids[key]);
+							this.scene.remove(this.asteroids[key]);
 							// Remove from tween
-							TWEEN.remove(this.tweenIds[key]);
+							this.TWEEN.remove(this.tweenIds[key]);
 
 							// Remove from arrays
-							this.asteroids = this.asteroids.filter(item => item !== key);
-							this.tweenIds = this.tweenIds.filter(item => item !== key);
+							this.asteroids = this.asteroids.filter((item: any) => item !== key);
+							this.tweenIds = this.tweenIds.filter((item: any) => item !== key);
 			        	}
 		        	}
 
 		        }
             },
-            ( xhr ) => {
+            ( xhr: any ) => {
                 // called while loading is progressing
                 // console.log( 'Asteroid loaded');
             },
-            ( error ) => {
+            ( error: any ) => {
                 // called when loading has errors
                 console.error( 'An error happened', error );
             },
         );
 	}
 
-	this.add = function(camera)
-	{
+	add(camera: any): void {
 		this.create(camera);
 	}
 
-	this.group = function()
-	{
+	group(): any {
 		return this.asteroids;
 	}
 
-	this.getTimer = function()
-	{
+	getTimer(): any {
 		return this.timer;
 	}
 
-	this.setTimer = function(timer = 200)
-	{
+	setTimer(timer = 200): void {
 		this.timer = timer;
 	}
 
-	this.spawner = function(camera)
-	{
+	spawner(camera: any): void {
         this.setTimer(this.getTimer() - 1);
         
         if(this.getTimer() <= 0) 
