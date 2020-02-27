5ae99f516f3ec93584efb553a59fda48636b7222
Diogenes Arruda <diogeneskelsen@gmail.com>
2020-02-27 14:26:58 +0000

    Update DebugDisplay.ts
    Refactoring to TS

--- src/component/DebugDisplay.ts
+++ src/component/DebugDisplay.ts
@@ -1,17 +1,20 @@
-function DebugDisplay(response)
+class DebugDisplay
 {
-	this.response = response;
+	response: any;
 
-	this.run = function()
-	{
+	constructor(response: any) {
+		this.response = response;
+
+		this.run();
+	}
+
+	run(): boolean {
 	    this.response['cube'].position.z -= 1;
 	    this.response['cube'].rotation.x += 0.05;
 	    this.response['cube'].rotation.y += 0.05;
+
+		return true;
 	}
-
-	this.run();
-
-	return true;
 }
 
 export default DebugDisplay;
